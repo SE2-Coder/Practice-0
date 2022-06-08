a8b0ac767c0e11dfab2c8bf9ff5706f6f0fe613f
SE2-Coder <criatiam.gutierrez@udea.edu.co>
2022-06-07 20:30:37 -0500

    vars declaration

--- p1.cpp
+++ p1.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main()
 {
-    cout << "hola mundo";
+  //I declare the variables for the first two triangles
+    int quantity = 26; //This variable does not indicate the number of characters in the pattern or diagram.
+    int letter = 65 + quantity - 1;//I use letter with the value 65, because it is the corresponding value in ascii for the letter 'a', which is the letter that all rows start with.
+    int rows_counter = 65;//I create this counter for the rows.
+    int space_counter = quantity - 1; //this counter works according to spaces.
+
+    /*==========================================================================================
+    I created a 'while loop to create the two patterns or triangles'
+    */
+
+return 0;
 }
